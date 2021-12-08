1c7b197c6a0766576f4913cbe3ea3c3a4cc07f9c
AlexandreSi <32449369+AlexandreSi@users.noreply.github.com>
2021-12-08 15:45:31 +0100

    Fix post build test

--- __tests__/post-build/exampleShortHeaders.json.spec.js
+++ __tests__/post-build/exampleShortHeaders.json.spec.js
@@ -31,7 +31,7 @@ describe('filters.json post build checks', () => {
       name: 'Platformer',
       authorIds: [],
       shortDescription:
-        'An example of a basic platformer (Mario-like) game made with GDevelop.',
+        'An example of a basic platformer (Mario-like) game. Jump everywhere and collect as many coins as possible!',
       license: 'MIT',
       previewImageUrls: [
         'https://resources.gdevelop-app.com/examples/platformer/thumbnail.png',
