31d7a21d42a633d27b606cc000ee48c3d22823ab
Tristan Rhodes <tristan.rhodes@gmail.com>
2022-07-21 09:09:48 -0600

    Spherez: New game template (#351)
    Shoot the blocks before they touch the ground.  Aim carefully to earn extra balls and power-ups.  What is the highest level that can you reach on the leaderboard?

--- scripts/generate-database.js
+++ scripts/generate-database.js
@@ -170,6 +170,7 @@ const checkProjectResourceFiles = async (project, projectFolderPath) => {
 
 const sortedStarterSlugs = new Set([
   'platformer',
+  'spherez',
   'absorbus',
   'tappy-plane',
   'parking-jam',
